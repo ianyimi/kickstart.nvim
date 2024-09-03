b3c46ee7064024bc82b1066119110d755f8df158
ianyimi <anyiminene@gmail.com>
2024-09-03 10:48:04 -0700

    feat: add format on save autocmd

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -2,8 +2,25 @@
 -- Default autocmds that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/autocmds.lua
 -- Add any additional autocmds here
 --
+local autocmd = vim.api.nvim_create_autocmd
+
+-- rewrite all augroups using tis function
+local function augroup(name)
+  return vim.api.nvim_create_augroup("sergio-lazyvim_" .. name, { clear = true })
+end
+
+-- augroup('formatonsave', { clear = true })
+autocmd("BufWritePre", {
+  group = augroup("formatonsave"),
+  desc = "Trigger format on save",
+  pattern = { "*.astro", "*.lua", "*.py", "*.js", "*.ts", "*.jsx", "*.tsx", "*.sh", "*.md" },
+  callback = function()
+    vim.lsp.buf.format()
+  end,
+})
+
 -- stop telescope from going into insert mode on close
-vim.api.nvim_create_autocmd({ "BufLeave", "BufWinLeave" }, {
+autocmd({ "BufLeave", "BufWinLeave" }, {
   callback = function(event)
     if vim.bo[event.buf].filetype == "TelescopePrompt" then
       vim.api.nvim_exec2("silent! stopinsert!", {})

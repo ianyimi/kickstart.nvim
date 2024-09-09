4d1e93757f745b43db8fcd49356acbb2e5c92024
Isaiah <anyiminene@gmail.com>
2024-09-09 10:21:34 -0700

    move back to mason lsp

--- lua/plugins/lsp-astro.lua
+++ lua/plugins/lsp-astro.lua
@@ -1,3 +1,10 @@
 return {
   "wuelnerdotexe/vim-astro",
+  config = function()
+    -- Enable TypeScript and TSX highlighting for .astro files
+    vim.g.astro_typescript = "enable"
+
+    -- Enable Stylus syntax for .astro files
+    vim.g.astro_stylus = "enable"
+  end,
 }
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -163,7 +163,7 @@ return {
       --    https://github.com/pmizio/typescript-tools.nvim
       --
       -- But for many setups, the LSP (`tsserver`) will work just fine
-      tsserver = {
+      ts_ls = {
         root_dir = require("lspconfig").util.root_pattern(".git"),
         single_file_support = false,
         settings = {

32c484e44289b5c8a377511a74266ef129cc98da
ianyimi <anyiminene@gmail.com>
2024-09-26 17:25:06 -0700

    copy autocmds, install treesitter

--- lua/config/options.lua
+++ lua/config/options.lua
@@ -6,6 +6,57 @@
 vim.g.mapleader = " "
 vim.g.maplocalleader = " "
 
+vim.opt.spelllang = { "en" }
+
+-- Fix markdown indentation settings
+vim.g.markdown_recommended_style = 0
+
+vim.g.autoformat = true
+
+-- LazyVim picker to use.
+-- Can be one of: telescope, fzf
+-- Leave it to "auto" to automatically use the picker
+-- enabled with `:LazyExtras`
+vim.g.lazyvim_picker = "auto"
+
+-- LazyVim root dir detection
+-- Each entry can be:
+-- * the name of a detector function like `lsp` or `cwd`
+-- * a pattern or array of patterns like `.git` or `lua`.
+-- * a function with signature `function(buf) -> string|string[]`
+vim.g.root_spec = { "lsp", { ".git", "lua" }, "cwd" }
+
+-- LazyVim automatically configures lazygit:
+--  * theme, based on the active colorscheme.
+--  * editPreset to nvim-remote
+--  * enables nerd font icons
+-- Set to false to disable.
+-- Set the options you want to override in `~/.config/lazygit/custom.yml`
+-- WARN: on Windows you might want to set `editPreset: "nvim"` due to
+-- this issue https://github.com/jesseduffield/lazygit/issues/3467
+vim.g.lazygit_config = true
+
+-- Options for the LazyVim statuscolumn
+vim.g.lazyvim_statuscolumn = {
+  folds_open = false, -- show fold sign when fold is open
+  folds_githl = false, -- highlight fold sign with git sign color
+}
+
+-- Optionally setup the terminal to use
+-- This sets `vim.o.shell` and does some additional configuration for:
+-- * pwsh
+-- * powershell
+-- LazyVim.terminal.setup("pwsh")
+
+-- Hide deprecation warnings
+vim.g.deprecation_warnings = false
+
+-- Set filetype to `bigfile` for files larger than 1.5 MB
+-- Only vim syntax will be enabled (with the correct filetype)
+-- LSP, treesitter and other ft plugins will be disabled.
+-- mini.animate will also be disabled.
+vim.g.bigfile_size = 1024 * 1024 * 1.5 -- 1.5 MB
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 -- Set Lazygit Options
@@ -25,6 +76,7 @@ vim.opt.completeopt = "menu,menuone,noselect"
 
 vim.opt.grepformat = "%f:%l:%c:%m"
 vim.opt.grepprg = "rg --vimgrep"
+vim.opt.smartcase = true -- Dont ignore case in words with capitals
 vim.opt.ignorecase = true -- Ignore case
 
 -- Save undo history
@@ -66,6 +118,7 @@ vim.opt.cursorcolumn = true
 vim.opt.scrolloff = 500
 
 vim.opt.tabstop = 2
+vim.opt.shiftround = true
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smarttab = true
@@ -73,11 +126,28 @@ vim.opt.autoindent = true
 vim.opt.smartindent = true
 vim.opt.breakindent = true
 
+vim.opt.formatexpr = "v:lua.require'lazyvim.util'.format.formatexpr()"
+vim.opt.formatoptions = "jcroqlnt" -- tcqj
+vim.opt.grepformat = "%f:%l:%c:%m"
+vim.opt.grepprg = "rg --vimgrep"
+vim.opt.ignorecase = true -- Ignore case
+vim.opt.inccommand = "nosplit" -- preview incremental substitute
+vim.opt.jumpoptions = "view"
+vim.opt.laststatus = 3 -- global statusline
+vim.opt.linebreak = true -- Wrap lines at convenient points
+vim.opt.list = true -- Show some invisible characters (tabs...
+vim.opt.mouse = "a" -- Enable mouse mode
+
 -- Show previous command in status bar
 vim.opt.showcmd = true
+vim.opt.spelloptions:append("noplainbuffer")
 
 vim.opt.wrap = false
 
+vim.opt.timeoutlen = vim.g.vscode and 1000 or 300 -- Lower than default (1000) to quickly trigger which-key
+vim.opt.undofile = true
+vim.opt.undolevels = 10000
+vim.opt.updatetime = 200 -- Save swap file and trigger CursorHold
 vim.opt.termguicolors = true
 
 vim.opt.backspace = { "indent,eol,start" }
@@ -87,5 +157,17 @@ vim.opt.path:append({ "**" })
 -- ignore node modules in searches
 vim.opt.wildignore:append({ "*/node_modules/*" })
 
-vim.opt.mouse = ""
 vim.opt.formatoptions:append({ "r" })
+
+vim.opt.winminwidth = 5 -- Minimum window width
+vim.opt.wrap = false -- Disable line wrap
+
+if vim.fn.has("nvim-0.10") == 1 then
+  vim.opt.smoothscroll = true
+  vim.opt.foldexpr = "v:lua.require'lazyvim.util'.ui.foldexpr()"
+  vim.opt.foldmethod = "expr"
+  vim.opt.foldtext = ""
+else
+  vim.opt.foldmethod = "indent"
+  vim.opt.foldtext = "v:lua.require'lazyvim.util'.ui.foldtext()"
+end

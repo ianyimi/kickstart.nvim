7d4673cbcc897f55af2a3f417aab4f193b61b42d
Isaiah <anyiminene@gmail.com>
2024-10-07 17:04:21 -0700

    leanConf feat: install harpoon and integrate with bufferline nvim. couldnt get working with telescope bc keybinds were not functioning

--- lua/plugins/editor/harpoon.lua
+++ lua/plugins/editor/harpoon.lua
@@ -1,26 +1,7 @@
--- basic telescope configuration
-local conf = require("telescope.config").values
-local function toggle_telescope(harpoon_files)
-	local file_paths = {}
-	for _, item in ipairs(harpoon_files.items) do
-		table.insert(file_paths, item.value)
-	end
-
-	require("telescope.pickers")
-		.new({}, {
-			prompt_title = "Harpoon",
-			finder = require("telescope.finders").new_table({
-				results = file_paths,
-			}),
-			previewer = conf.file_previewer({}),
-			sorter = conf.generic_sorter({}),
-		})
-		:find()
-end
-
 return {
 	"ThePrimeagen/harpoon",
 	branch = "harpoon2",
+	lazy = false,
 	opts = {
 		menu = {
 			width = vim.api.nvim_win_get_width(0) - 4,
@@ -30,19 +11,90 @@ return {
 		},
 	},
 	keys = function()
+		local harpoon = require("harpoon")
+		-- basic telescope configuration
+		local conf = require("telescope.config").values
+		local function toggle_telescope(harpoon_files)
+			local file_paths = {}
+			for _, item in ipairs(harpoon_files.items) do
+				table.insert(file_paths, item.value)
+			end
+
+			local make_finder = function()
+				local paths = {}
+
+				for _, item in ipairs(harpoon_files.items) do
+					table.insert(paths, item.value)
+				end
+
+				return require("telescope.finders").new_table({
+					results = paths,
+				})
+			end
+
+			require("telescope.pickers")
+				.new({}, {
+					prompt_title = "Harpoon",
+					finder = make_finder(),
+					previewer = conf.file_previewer({}),
+					sorter = conf.generic_sorter({}),
+					attach_mappings = function(prompt_bufnr, map)
+						local actions = require("telescope.actions")
+						local action_state = require("telescope.actions.state")
+						local current_picker = action_state.get_current_picker(prompt_bufnr)
+						local ok, mark = pcall(require, "harpoon.mark")
+						if not ok then
+							return true
+						end
+
+						local function delete_mark()
+							local selection = action_state.get_selected_entry()
+							if selection == nil then
+								return
+							end
+
+							harpoon:list():removeAt(selection.index)
+							current_picker:refresh(make_finder())
+							-- -- Find the index of the selected file in Harpoon's marks
+							-- local harpoon_marks = mark.get_marked_file_list()
+							-- local target_index = nil
+							-- for idx, entry in ipairs(harpoon_marks) do
+							-- 	if entry.filename == selection.value then
+							-- 		target_index = idx
+							-- 		break
+							-- 	end
+							-- end
+							--
+							-- if target_index then
+							-- 	mark.rm_file(target_index)
+							--
+							-- 	local picker = action_state.get_current_picker(prompt_bufnr)
+							-- 	picker:refresh(make_finder(), { reset_prompt = true })
+							--
+							-- 	require("bufferline").refresh()
+							-- end
+						end
+
+						map("i", "<C-d>", delete_mark)
+						map("n", "<C-d>", delete_mark)
+						return true
+					end,
+				})
+				:find()
+		end
+
 		local keys = {
 			{
 				"<leader>H",
 				function()
-					require("harpoon"):list():add()
+					harpoon:list():add()
 				end,
 				desc = "Harpoon File",
 			},
 			{
 				"<leader>h",
 				function()
-					local harpoon = require("harpoon")
-					toggle_telescope(harpoon:list())
+					harpoon.ui:toggle_quick_menu(harpoon:list())
 				end,
 				desc = "Harpoon Quick Menu",
 			},
@@ -52,7 +104,7 @@ return {
 			table.insert(keys, {
 				"<leader>" .. i,
 				function()
-					require("harpoon"):list():select(i)
+					harpoon:list():select(i)
 				end,
 				desc = "Harpoon to File " .. i,
 			})
--- lua/plugins/ui/bufferline.lua
+++ lua/plugins/ui/bufferline.lua
@@ -1,49 +1,111 @@
 return {
-  "akinsho/bufferline.nvim",
-  event = "VeryLazy",
-  keys = {
-    { "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
-    { "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
-    { "<leader>bxo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
-    { "<leader>bxr", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
-    { "<leader>bxl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
-    { "<leader>br", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
-    { "<leader>bl", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
-    { "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-    { "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-    -- { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
-    -- { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
-  },
-  opts = {
-    options = {
-      -- stylua: ignore
-      close_command = function(n) LazyVim.ui.bufremove(n) end,
-      -- stylua: ignore
-      right_mouse_command = function(n) LazyVim.ui.bufremove(n) end,
-      diagnostics = "nvim_lsp",
-      always_show_bufferline = true,
-      diagnostics_indicator = function(_, _, diag)
-        local icons = LazyVim.config.icons.diagnostics
-        local ret = (diag.error and icons.Error .. diag.error .. " " or "")
-          .. (diag.warning and icons.Warn .. diag.warning or "")
-        return vim.trim(ret)
-      end,
-      ---@param opts bufferline.IconFetcherOpts
-      get_element_icon = function(opts)
-        return LazyVim.config.icons.ft[opts.filetype]
-      end,
-    },
-  },
-  config = function(_, opts)
-    require("bufferline").setup(opts)
-    -- Fix bufferline when restoring a session
-    vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
-      callback = function()
-        vim.schedule(function()
-          pcall(nvim_bufferline)
-        end)
-      end,
-    })
-  end,
-}
+	"akinsho/bufferline.nvim",
+	event = "VeryLazy",
+	dependencies = { "ThePrimeagen/harpoon", lazy = false },
+	keys = {
+		{ "<leader>bp", "<Cmd>BufferLineTogglePin<CR>", desc = "Toggle Pin" },
+		{ "<leader>bP", "<Cmd>BufferLineGroupClose ungrouped<CR>", desc = "Delete Non-Pinned Buffers" },
+		{ "<leader>bxo", "<Cmd>BufferLineCloseOthers<CR>", desc = "Delete Other Buffers" },
+		{ "<leader>bxr", "<Cmd>BufferLineCloseRight<CR>", desc = "Delete Buffers to the Right" },
+		{ "<leader>bxl", "<Cmd>BufferLineCloseLeft<CR>", desc = "Delete Buffers to the Left" },
+		{ "<leader>br", "<cmd>BufferLineMovePrev<cr>", desc = "Move buffer prev" },
+		{ "<leader>bl", "<cmd>BufferLineMoveNext<cr>", desc = "Move buffer next" },
+		{ "<S-h>", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+		{ "<S-l>", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+		-- { "[b", "<cmd>BufferLineCyclePrev<cr>", desc = "Prev Buffer" },
+		-- { "]b", "<cmd>BufferLineCycleNext<cr>", desc = "Next Buffer" },
+	},
+	opts = function()
+		local function get_marked_buffers()
+			local ok, mark = pcall(require, "harpoon.mark")
+			if not ok then
+				return {}
+			end
+			local harpoon_buffers = {}
+			local current_idx = 1
 
+			while mark.get_marked_file_name(current_idx) do
+				harpoon_buffers[current_idx] = mark.get_marked_file_name(current_idx)
+				current_idx = current_idx + 1
+			end
+
+			return harpoon_buffers
+		end
+
+		local function get_index_of_value(table, value)
+			local index = nil
+			for i, v in pairs(table) do
+				if v == value then
+					index = i
+					break
+				end
+			end
+
+			return index
+		end
+
+		return {
+			options = {
+				-- stylua: ignore
+				close_command = function(n) LazyVim.ui.bufremove(n) end,
+				-- stylua: ignore
+				right_mouse_command = function(n) LazyVim.ui.bufremove(n) end,
+				diagnostics = "nvim_lsp",
+				always_show_bufferline = true,
+				diagnostics_indicator = function(_, _, diag)
+					local icons = LazyVim.config.icons.diagnostics
+					local ret = (diag.error and icons.Error .. diag.error .. " " or "")
+						.. (diag.warning and icons.Warn .. diag.warning or "")
+					return vim.trim(ret)
+				end,
+				---@param opts bufferline.IconFetcherOpts
+				get_element_icon = function(opts)
+					return LazyVim.config.icons.ft[opts.filetype]
+				end,
+				numbers = function(buffer)
+					-- get the harpoon buffers and the name of the buffer
+					local buf_name = vim.fn.bufname(buffer.id)
+					local harpoon_buffers = get_marked_buffers()
+
+					return get_index_of_value(harpoon_buffers, buf_name) or ""
+				end,
+				sort_by = function(buffer_a, buffer_b)
+					-- get the harpoon buffers and the names of the buffers
+					local harpoon_buffers = get_marked_buffers()
+					local buf_name_a = vim.fn.bufname(buffer_a.id)
+					local buf_name_b = vim.fn.bufname(buffer_b.id)
+
+					local idx_a = get_index_of_value(harpoon_buffers, buf_name_a)
+					local idx_b = get_index_of_value(harpoon_buffers, buf_name_b)
+
+					if idx_a and idx_b then
+						-- Both buffers are Harpoon files; sort by their Harpoon index
+						return idx_a < idx_b
+					elseif idx_a then
+						-- Only buffer_a is a Harpoon file; it comes first
+						return true
+					elseif idx_b then
+						-- Only buffer_b is a Harpoon file; it comes first
+						return false
+					else
+						-- Neither buffer is a Harpoon file; sort by buffer ID or name
+						return buffer_a.id < buffer_b.id
+						-- Alternatively, sort by name:
+						-- return buf_name_a < buf_name_b
+					end
+				end,
+			},
+		}
+	end,
+	config = function(_, opts)
+		require("bufferline").setup(opts)
+		-- Fix bufferline when restoring a session
+		vim.api.nvim_create_autocmd({ "BufAdd", "BufDelete" }, {
+			callback = function()
+				vim.schedule(function()
+					pcall(require("bufferline").refresh)
+				end)
+			end,
+		})
+	end,
+}

c3cfea8eb819e736c6dbd2aeac4ecab275588051
Isaiah <anyiminene@gmail.com>
2024-10-11 11:45:40 -0700

    leanConf chore: update worktree config so that oil nvim is not required on startup by git-worktree

--- lua/plugins/editor/git-worktree.lua
+++ lua/plugins/editor/git-worktree.lua
@@ -1,17 +1,14 @@
 return {
 	"ThePrimeagen/git-worktree.nvim",
-	dependencies = {
-		"stevearc/oil.nvim"
-	},
 	opts = function()
 		local worktree = require("git-worktree")
-		local oil_ok, oil = pcall(require, "oil")
-		local mini_files_ok, mini_files = pcall(require, "mini.files")
 		-- Function to normalize paths to use backslashes
     local function normalize_path(path)
       return path:gsub("/", "\\")
     end
 		worktree.on_tree_change(function(op, metadata)
+			local oil_ok, oil = pcall(require, "oil")
+			local mini_files_ok, mini_files = pcall(require, "mini.files")
 			if op == worktree.Operations.Switch then
 				local new_path = normalize_path(metadata.path)
 				vim.api.nvim_set_current_dir(new_path)
@@ -20,7 +17,7 @@ return {
 				elseif mini_files_ok then
 					mini_files.open(new_path)
 				end
-				print("Updated Directory: " .. metadata.prev_path .. " > " .. new_path)
+				print("Updated Directory: " .. new_path)
 			end
 		end)
 		return {}

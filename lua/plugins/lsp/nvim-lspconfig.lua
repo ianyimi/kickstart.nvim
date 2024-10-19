a2c7645af7ba6874fddac52b28f8227de8f41de1
Isaiah <anyiminene@gmail.com>
2024-10-19 03:00:25 -0700

    leanConf feat: install yaml lsp config

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -315,6 +315,42 @@ return {
 						},
 					},
 				},
+				yamlls = {
+					-- Have to add this for yamlls to understand that we support line folding
+					capabilities = {
+						textDocument = {
+							foldingRange = {
+								dynamicRegistration = false,
+								lineFoldingOnly = true,
+							},
+						},
+					},
+					-- lazy-load schemastore when needed
+					on_new_config = function(new_config)
+						new_config.settings.yaml.schemas = vim.tbl_deep_extend(
+						"force",
+						new_config.settings.yaml.schemas or {},
+						require("schemastore").yaml.schemas()
+						)
+					end,
+					settings = {
+						redhat = { telemetry = { enabled = false } },
+						yaml = {
+							keyOrdering = false,
+							format = {
+								enable = true,
+							},
+							validate = true,
+							schemaStore = {
+								-- Must disable built-in schemaStore support to use
+								-- schemas from SchemaStore.nvim plugin
+								enable = false,
+								-- Avoid TypeError: Cannot read properties of undefined (reading 'length')
+								url = "",
+							},
+						},
+					},
+				},
 			}
 
 			LazyVim.extend(servers.vtsls, "settings.vtsls.tsserver.globalPlugins", {
@@ -353,5 +389,10 @@ return {
 				},
 			})
 		end,
+	},
+	{
+		"b0o/SchemaStore.nvim",
+		lazy = true,
+		version = false, -- last release is way too old
 	},
 }

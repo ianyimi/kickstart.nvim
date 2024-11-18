108eac64d3bab10e08cd33fc00a421c372d8bf86
Isaiah <anyiminene@gmail.com>
2024-11-18 01:05:22 -0800

    feat: get tailwind tools working with autosorting classes and auto displaying color previews, set autoformat but still need to update autoformat to read prettierconfig js file

--- lua/plugins/lsp/nvim-lspconfig.lua
+++ lua/plugins/lsp/nvim-lspconfig.lua
@@ -1,142 +1,166 @@
 return {
-  "VonHeikemen/lsp-zero.nvim",
-  branch = "v2.x",
-  dependencies = {
-    -- LSP Support
-    { "neovim/nvim-lspconfig" }, -- Required
-    { -- Optional
-      "williamboman/mason.nvim",
-      build = function()
-        pcall(vim.cmd, "MasonUpdate")
-      end,
-    },
-    { "williamboman/mason-lspconfig.nvim" }, -- Optional
-		{ 'j-hui/fidget.nvim', opts = {} },
+	"VonHeikemen/lsp-zero.nvim",
+	branch = "v2.x",
+	dependencies = {
+		-- LSP Support
+		{ "neovim/nvim-lspconfig" }, -- Required
+		{ -- Optional
+			"williamboman/mason.nvim",
+			build = function()
+				pcall(vim.cmd, "MasonUpdate")
+			end,
+		},
+		{ "williamboman/mason-lspconfig.nvim" }, -- Optional
+		{ "j-hui/fidget.nvim", opts = {} },
 
-    -- Autocompletion
-    { "hrsh7th/nvim-cmp" }, -- Required
-    { "hrsh7th/cmp-nvim-lsp" }, -- Required
-    { "L3MON4D3/LuaSnip" }, -- Required
-    { "rafamadriz/friendly-snippets" },
-    { "hrsh7th/cmp-buffer" },
-    { "hrsh7th/cmp-path" },
-    { "hrsh7th/cmp-cmdline" },
-    { "saadparwaiz1/cmp_luasnip" },
-  },
-  config = function()
-    local lsp = require("lsp-zero")
+		-- Tailwind Tools
+		{ "luckasRanarison/tailwind-tools.nvim" },
+		{ "onsails/lspkind-nvim" },
 
-    lsp.on_attach(function(client, bufnr)
+		-- Autocompletion
+		{ "hrsh7th/nvim-cmp" }, -- Required
+		{ "hrsh7th/cmp-nvim-lsp" }, -- Required
+		{ "L3MON4D3/LuaSnip" }, -- Required
+		{ "rafamadriz/friendly-snippets" },
+		{ "hrsh7th/cmp-buffer" },
+		{ "hrsh7th/cmp-path" },
+		{ "hrsh7th/cmp-cmdline" },
+		{ "saadparwaiz1/cmp_luasnip" },
+	},
+	config = function()
+		local lsp = require("lsp-zero")
+
+		lsp.on_attach(function(client, bufnr)
 			local map = function(keys, func, desc)
-				vim.keymap.set('n', keys, func, { buffer = buffer, desc = 'LSP: ' .. desc })
+				vim.keymap.set("n", keys, func, { buffer = buffer, desc = "LSP: " .. desc })
 			end
 
 			-- Key mappings
-			map('gh', vim.lsp.buf.hover, 'Preview Hover')
-			map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
-			map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-			map('<leader>cd', vim.diagnostic.open_float, '[S]how [D]iagnostic')
-			map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
-			map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
-			map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
-			map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
-			map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
-			map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
-			map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
+			map("gh", vim.lsp.buf.hover, "Preview Hover")
+			map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
+			map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+			map("<leader>cd", vim.diagnostic.open_float, "[S]how [D]iagnostic")
+			map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
+			map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
+			map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
+			map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
+			map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+			map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+			map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
 			-- Highlight references
-			if client.supports_method('textDocument/documentHighlight') then
-				local highlight_augroup = vim.api.nvim_create_augroup('lsp_document_highlight', { clear = true })
+			if client.supports_method("textDocument/documentHighlight") then
+				local highlight_augroup = vim.api.nvim_create_augroup("lsp_document_highlight", { clear = true })
 				vim.api.nvim_clear_autocmds({ group = highlight_augroup, buffer = bufnr })
-				vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
+				vim.api.nvim_create_autocmd({ "CursorHold", "CursorHoldI" }, {
 					group = highlight_augroup,
 					buffer = bufnr,
 					callback = vim.lsp.buf.document_highlight,
 				})
-				vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
+				vim.api.nvim_create_autocmd({ "CursorMoved", "CursorMovedI" }, {
 					group = highlight_augroup,
 					buffer = bufnr,
 					callback = vim.lsp.buf.clear_references,
 				})
 			end
-    end)
+		end)
 
-    require("mason").setup({})
-    require("mason-lspconfig").setup({
-      ensure_installed = {
-        "astro",
-        "bashls",
+		require("mason").setup({})
+		require("mason-lspconfig").setup({
+			ensure_installed = {
+				"astro",
+				"bashls",
 				"clangd",
-        "dockerls",
-        "eslint",
-        "gopls",
-        "html",
-        "jsonls",
-        "lua_ls",
-        "tailwindcss",
+				"dockerls",
+				"eslint",
+				"gopls",
+				"html",
+				"jsonls",
+				"lua_ls",
+				"tailwindcss",
 				"taplo",
-        "vtsls",
-      },
-      handlers = {
-        lsp.default_setup,
-        lua_ls = function()
-          local lua_opts = lsp.nvim_lua_ls()
-          require("lspconfig").lua_ls.setup(lua_opts)
-        end,
-      },
-    })
+				"vtsls",
+			},
+			handlers = {
+				lsp.default_setup,
+				lua_ls = function()
+					local lua_opts = lsp.nvim_lua_ls()
+					require("lspconfig").lua_ls.setup(lua_opts)
+				end,
+			},
+		})
 
-    local cmp_action = require("lsp-zero").cmp_action()
-    local cmp = require("cmp")
-    local cmp_select = { behavior = cmp.SelectBehavior.Select }
+		local cmp_action = require("lsp-zero").cmp_action()
+		local cmp = require("cmp")
+		local cmp_select = { behavior = cmp.SelectBehavior.Select }
 
-    require("luasnip.loaders.from_vscode").lazy_load()
+		require("luasnip.loaders.from_vscode").lazy_load()
 
-    -- `/` cmdline setup.
-    cmp.setup.cmdline("/", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = {
-        { name = "buffer" },
-      },
-    })
+		-- `/` cmdline setup.
+		cmp.setup.cmdline("/", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = {
+				{ name = "buffer" },
+			},
+		})
 
-    -- `:` cmdline setup.
-    cmp.setup.cmdline(":", {
-      mapping = cmp.mapping.preset.cmdline(),
-      sources = cmp.config.sources({
-        { name = "path" },
-      }, {
-        {
-          name = "cmdline",
-          option = {
-            ignore_cmds = { "Man", "!" },
-          },
-        },
-      }),
-    })
+		-- `:` cmdline setup.
+		cmp.setup.cmdline(":", {
+			mapping = cmp.mapping.preset.cmdline(),
+			sources = cmp.config.sources({
+				{ name = "path" },
+			}, {
+				{
+					name = "cmdline",
+					option = {
+						ignore_cmds = { "Man", "!" },
+					},
+				},
+			}),
+		})
 
-    cmp.setup({
-      snippet = {
-        expand = function(args)
-          require("luasnip").lsp_expand(args.body)
-        end,
-      },
-      sources = {
-        { name = "nvim_lsp" },
-        { name = "luasnip", keyword_length = 2 },
-        { name = "buffer", keyword_length = 3 },
-        { name = "path" },
-      },
-      mapping = cmp.mapping.preset.insert({
-        ["<C-p>"] = cmp.mapping.select_prev_item(cmp_select),
-        ["<C-n>"] = cmp.mapping.select_next_item(cmp_select),
-        ["<CR>"] = cmp.mapping.confirm({ select = true }),
-        ["<C-Space>"] = cmp.mapping.complete(),
-        ["<C-f>"] = cmp_action.luasnip_jump_forward(),
-        ["<C-b>"] = cmp_action.luasnip_jump_backward(),
-        ["<Tab>"] = cmp_action.luasnip_supertab(),
-        ["<S-Tab>"] = cmp_action.luasnip_shift_supertab(),
-      }),
-    })
-  end,
+		cmp.setup({
+			snippet = {
+				expand = function(args)
+					require("luasnip").lsp_expand(args.body)
+				end,
+			},
+			sources = {
+				{ name = "nvim_lsp" },
+				{ name = "luasnip", keyword_length = 2 },
+				{ name = "buffer", keyword_length = 3 },
+				{ name = "path" },
+			},
+			formatting = {
+				format = require("lspkind").cmp_format({
+					before = require("tailwind-tools.cmp").lspkind_format,
+				}),
+			},
+			mapping = cmp.mapping.preset.insert({
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif require("luasnip").expand_or_jumpable() then
+						require("luasnip").expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<S-Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_prev_item()
+					elseif require("luasnip").jumpable(-1) then
+						require("luasnip").jump(-1)
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
+				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<Esc>"] = cmp.mapping(function(fallback)
+					cmp.mapping.abort()
+					vim.cmd("stopinsert")
+				end, { "i", "s" }),
+			}),
+		})
+	end,
 }

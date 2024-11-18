fa4389183b01d8b8bcf29c2b0009f632250cae11
Isaiah <anyiminene@gmail.com>
2024-11-17 22:37:18 -0800

    feat: added format on save with conform nvim working

--- lua/plugins/formatting/conform.lua
+++ lua/plugins/formatting/conform.lua
@@ -21,11 +21,11 @@ return {
 				lua = { "stylua" },
 				python = { "isort", "black" },
 			},
-			-- format_on_save = {
-			-- 	lsp_fallback = true,
-			-- 	async = false,
-			-- 	timeout_ms = 1000,
-			-- },
+			format_on_save = {
+				lsp_fallback = true,
+				async = false,
+				timeout_ms = 1000,
+			},
 		})
 
 		-- Configure individual formatters
@@ -43,12 +43,12 @@ return {
 			prepend_args = { "-i", "4" },
 		}
 
-		vim.keymap.set({ "n", "v" }, "<leader>mp", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
+		-- vim.keymap.set({ "n", "v" }, "<leader>mp", function()
+		-- 	conform.format({
+		-- 		lsp_fallback = true,
+		-- 		async = false,
+		-- 		timeout_ms = 1000,
+		-- 	})
+		-- end, { desc = "Format file or range (in visual mode)" })
 	end,
 }
